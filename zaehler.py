407130f1b9145ac8c8b1f9fe744d1aeac7d7fa7b
paddy287 <patrick.heinzler@gmx.de>
2022-02-11 15:16:59 +0100

    gui started

---
+++ zaehler.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import tkinter as tk
+from tkinter import ttk
+
+class Main(tk.Frame):
+    def __init__(self, master =None):
+        tk.Frame.__init__(self, master)
+        self.root = master       
+        self.create_widgets()
+        self.count = 0
+    
+    def create_widgets(self):
+        def cnt():
+            self.lable = tk.Label(self.root, text= str(self.count))
+            self.count +=1
+            self.lable.grid(row=0, column=0)
+        self.button = tk.Button(self.root, command= cnt, text="Count")
+        self.button.grid(row=1, column= 0)
+if __name__ == '__main__':
+    root = tk.Tk()
+    main = Main(root)
+    root.mainloop()
